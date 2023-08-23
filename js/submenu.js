09bda3274841617952847d47f7a7be19450d3d54
leonelvalentinn <leonelvalentinn@gmail.com>
2023-08-23 15:22:38 -0600

    Secciones de servicios completas

--- js/submenu.js
+++ js/submenu.js
@@ -1,12 +1,8 @@
-let listElements = document.querySelectorAll(".submenu");
+const listElements = document.getElementById("menu");
 let btn = document.getElementById("servicios-mov");
 
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", );
 
-    let height = 0;
-    if (listElements.clientHeight == "0") {
-      height = listElements.scrollHeight;
-    }
-
-    listElements.style.height = `${height}px`;
-  });
+  function top() {
+    listElements.classList.add("top");
+  }
